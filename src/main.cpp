c1397339db258dfc4a2f4c64490fe87633baa9ea
Øyvind Jægtnes <oyvind@jaegtnes.no>
2025-03-25 20:47:32 +0100

    Check google drive folder for changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#include <chrono>
 #include <dpp/dpp.h>
 #include <format>
 #include <inicpp.h>
 #include <ollama.hpp>
-#include <regex>
 #include <stdlib.h>
 #include <string>
 
@@ -14,6 +14,7 @@ int main() {
     return -1;
   }
 
+  // Read the configuration parameters from ini file
   ini::IniFile config;
   config.setMultiLineValues(true);
   config.load(std::format("{}/.config/nissefar/config", home));
@@ -25,6 +26,20 @@ int main() {
       config["General"]["text_model"].as<std::string>();
   const std::string vision_model =
       config["General"]["vision_model"].as<std::string>();
+  const std::string google_api =
+      config["General"]["google_api"].as<std::string>();
+
+  // Map to store the file metadata for TB test
+
+  std::map<std::string, std::chrono::sys_time<std::chrono::milliseconds>>
+      tbfiles;
+
+  // Create url to fetch file data
+  const std::string directory_url =
+      std::format("https://www.googleapis.com/drive/v3/"
+                  "files?q='1HOwktdiZmm40atGPwymzrxErMi1ZrKPP'+in+parents&"
+                  "key={}&fields=files(id,name,modifiedTime,webViewLink)",
+                  google_api);
 
   dpp::cluster bot(token, dpp::i_default_intents | dpp::i_message_content);
   bot.on_log(dpp::utility::cout_logger());
@@ -34,6 +49,106 @@ int main() {
 
   ollama::setReadTimeout(360);
   ollama::setWriteTimeout(360);
+
+  // Set opp en timer som fyrer av hvert minutt
+  bot.on_ready([&bot, &directory_url, &text_model,
+                &tbfiles](const dpp::ready_t &event) -> dpp::task<void> {
+    // Hent katalogdata fra google drive, setup pull
+    dpp::http_request_completion_t response =
+        co_await bot.co_request(directory_url, dpp::m_get);
+    bot.log(dpp::ll_info,
+            std::format("json data size: {}", response.body.size()));
+
+    nlohmann::json directory_data = nlohmann::json::parse(response.body.data());
+
+    // Loop over filene og hent ut filnavn og parse tid
+    for (auto filedata : directory_data["files"]) {
+
+      // try to parse the data
+      // 2024-09-19T07:45:19.173Z
+
+      std::string datestring = filedata["modifiedTime"].front();
+      std::string filename = filedata["name"].front();
+      std::string datestringparse =
+          datestring.substr(0, 10) + " " + datestring.substr(12, 8);
+
+      std::chrono::sys_time<std::chrono::milliseconds> tp;
+      std::istringstream ds(datestringparse);
+
+      // Dersom parse er ok, stapp i map for senere bruk
+      ds >> std::chrono::parse("%Y-%m-%d %H:%M:%S", tp);
+      if (ds.fail()) {
+        bot.log(dpp::ll_error, std::format("Error parsing date: {}", ds.str()));
+      } else {
+        bot.log(dpp::ll_info, std::format("json test: {}, {}, {}", filename,
+                                          datestring, tp.time_since_epoch()));
+        tbfiles[filename] = tp;
+      }
+    }
+
+    // Set opp timeren som skal sjekke filer hvert 5. minutt
+    bot.start_timer(
+        [&bot, &tbfiles, &directory_url, &text_model](const dpp::timer &timer) {
+          bot.log(dpp::loglevel::ll_info, std::format("Check files: 300 sec"));
+
+          // Hent katalogdata, denne gangen async med callback lambda
+          bot.request(
+              directory_url, dpp::m_get,
+              [&tbfiles, &bot,
+               &text_model](const dpp::http_request_completion_t &response) {
+                bot.log(dpp::ll_info, std::format("json data size: {}",
+                                                  response.body.size()));
+                nlohmann::json directory_data =
+                    nlohmann::json::parse(response.body.data());
+
+                // Loop over fildata og hent ut tid og navn
+                for (auto filedata : directory_data["files"]) {
+                  std::string datestring = filedata["modifiedTime"].front();
+                  std::string filename = filedata["name"].front();
+                  std::string datestringparse =
+                      datestring.substr(0, 10) + " " + datestring.substr(12, 8);
+                  std::chrono::sys_time<std::chrono::milliseconds> tp;
+                  std::istringstream ds(datestringparse);
+
+                  // Dersom datoene parser, sjekk mot map om de har endret seg
+                  // og log / lagre ny tid dersom de er endret
+                  ds >> std::chrono::parse("%Y-%m-%d %H:%M:%S", tp);
+                  if (ds.fail()) {
+                    bot.log(dpp::ll_error,
+                            std::format("Error parsing date: {}", ds.str()));
+                  } else {
+                    if (tbfiles[filename] < tp) {
+                      bot.log(dpp::ll_info,
+                              std::format("File {} has changed", filename));
+                      std::string message_text;
+
+                      try {
+                        message_text = ollama::generate(
+                            text_model,
+                            std::format(
+                                "Make a witty comment about that Bjørn just "
+                                "updated the following google shiiiet file: {}",
+                                filename));
+                      } catch (ollama::exception &e) {
+                        message_text =
+                            std::format("Exception running llm: {}. But the "
+                                        "file {} has been updated by Bjørn",
+                                        e.what(), filename);
+                      }
+                      std::string drive_url = filedata["webViewLink"].front();
+                      message_text.append(std::format("\n{}", drive_url));
+                      tbfiles[filename] = tp;
+                      bot.log(dpp::ll_info,
+                              std::format("Bot answer: {}", message_text));
+                      dpp::message msg(1267731118895927347, message_text);
+                      bot.message_create(msg);
+                    }
+                  }
+                }
+              });
+        },
+        300);
+  });
 
   // Vi ønsker kun å svare på melding til boten i spesifikke kanaler
 
